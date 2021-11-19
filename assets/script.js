b66c96b52f31041b2985c3298d625f6c301471e5
Jesus <jesusbalandra@outlook.com>
2021-11-18 18:05:56 -0700

    updated the JS to moment.js added onto project

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,46 @@
-// created variables for current date and to be displayed onto page
-n = new Date();
-y = n.getFullYear();
-m = n.getMonth()+1;
-d = n.getDate();
-document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
+$(document).ready(function() {
+
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+    $(".saveBtn").on("click", function() {
+        console.log("this");
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
+
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+
+function hourTracker() {
+    var currentHour = moment().hour();
+
+    $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+hourTracker();
+})
