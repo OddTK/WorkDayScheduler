e0105386b00cc907041106d6e895501eba2a4af8
Jesus <jesusbalandra@outlook.com>
2021-11-19 12:12:01 -0700

    added comments explaining my JS

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
 
+    // using moment.js to pull current day and time
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
     $(".saveBtn").on("click", function() {
         console.log("this");
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
-
+        // saving time and text to local storage
         localStorage.setItem(time, text);
     })
-
+//get data from local storage
 $("#hour9 .description").val(localStorage.getItem("hour9"));
 $("#hour10 .description").val(localStorage.getItem("hour10"));
 $("#hour11 .description").val(localStorage.getItem("hour11"));
@@ -22,9 +23,11 @@ $("#hour17 .description").val(localStorage.getItem("hour17"));
 function hourTracker() {
     var currentHour = moment().hour();
 
+    //setting loop for time block
     $(".time-block").each(function() {
         var blockHour = parseInt($(this).attr("id").split("hour")[1]);
 
+        //check the current time, will set color set in CSS and HTML
         if (blockHour < currentHour) {
             $(this).addClass("past");
             $(this).removeClass("future");
